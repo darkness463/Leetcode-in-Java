7df4161d329afbb4e91d29ac5ddbfd719f3cdea4
Darkness463 <darkness463@gmail.com>
2019-03-25 16:59:30 +0800

    add: subdomain-visit-count.

---
+++ src/com/darkness463/leetcode/string/SubdomainVisitCount.java
@@ -0,0 +1,100 @@
+package com.darkness463.leetcode.string;
+
+import java.util.*;
+
+/**
+ * 811. 子域名访问计数
+ * 一个网站域名，如"discuss.leetcode.com"，包含了多个子域名。作为顶级域名，常用的有"com"，下一级则有"leetcode.com"，最低的一级为"discuss.leetcode.com"。当我们访问域名"discuss.leetcode.com"时，也同时访问了其父域名"leetcode.com"以及顶级域名 "com"。
+ *
+ * 给定一个带访问次数和域名的组合，要求分别计算每个域名被访问的次数。其格式为访问次数+空格+地址，例如："9001 discuss.leetcode.com"。
+ *
+ * 接下来会给出一组访问次数和域名组合的列表cpdomains 。要求解析出所有域名的访问次数，输出格式和输入格式相同，不限定先后顺序。
+ *
+ * 示例 1:
+ * 输入:
+ * ["9001 discuss.leetcode.com"]
+ * 输出:
+ * ["9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"]
+ * 说明:
+ * 例子中仅包含一个网站域名："discuss.leetcode.com"。按照前文假设，子域名"leetcode.com"和"com"都会被访问，所以它们都被访问了9001次。
+ * 示例 2
+ * 输入:
+ * ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+ * 输出:
+ * ["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
+ * 说明:
+ * 按照假设，会访问"google.mail.com" 900次，"yahoo.com" 50次，"intel.mail.com" 1次，"wiki.org" 5次。
+ * 而对于父域名，会访问"mail.com" 900+1 = 901次，"com" 900 + 50 + 1 = 951次，和 "org" 5 次。
+ * 注意事项：
+ *
+ *  cpdomains 的长度小于 100。
+ * 每个域名的长度小于100。
+ * 每个域名地址包含一个或两个"."符号。
+ * 输入中任意一个域名的访问次数都小于10000。
+ *
+ * https://leetcode-cn.com/problems/subdomain-visit-count/
+ * @author <a href="mailto:darkness463@gmail.com">Darkness463</a>
+ */
+public class SubdomainVisitCount {
+
+    public List<String> subdomainVisits(String[] cpdomains) {
+        List<String> result = new ArrayList<>();
+        if (cpdomains == null || cpdomains.length == 0) {
+            return result;
+        }
+
+        Map<String, Integer> counts = new HashMap<>();
+        for (String domains : cpdomains) {
+            String[] sp1 = domains.split(" ");
+            Integer cnt = Integer.parseInt(sp1[0]);
+            String[] sp2 = sp1[1].split("\\.");
+            for (int i = sp2.length - 1; i >= 0; i--) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(sp2[i]);
+                int j = i;
+                while (++j < sp2.length) {
+                    sb.append(".").append(sp2[j]);
+                }
+                String domain = sb.toString();
+                Integer count = counts.getOrDefault(domain, 0);
+                count += cnt;
+                counts.put(domain, count);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+            result.add(entry.getValue() + " " + entry.getKey());
+        }
+        return result;
+    }
+
+    /**
+     * faster.
+     */
+    public List<String> subdomainVisits2(String[] cpdomains) {
+        List<String> result = new ArrayList<>();
+        if (cpdomains == null || cpdomains.length == 0) {
+            return result;
+        }
+
+        Map<String, Integer> counts = new HashMap<>();
+        for (String domains : cpdomains) {
+            String[] sp1 = domains.split(" ");
+            Integer cnt = Integer.parseInt(sp1[0]);
+            String domain = sp1[1];
+            int index = -1;
+            do {
+                domain = domain.substring(index + 1);
+                Integer count = counts.getOrDefault(domain, 0);
+                count += cnt;
+                counts.put(domain, count);
+            } while ((index = domain.indexOf(".")) != -1);
+        }
+
+        for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+            result.add(entry.getValue() + " " + entry.getKey());
+        }
+        return result;
+    }
+
+}
